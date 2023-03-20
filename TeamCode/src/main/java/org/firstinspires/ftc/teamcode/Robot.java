a8af1572cc8a8db85f32bba953a45d48db455869
Slipperee-CODE <86428966+Slipperee-CODE@users.noreply.github.com>
2023-03-19 22:24:23 -0700

    TODO - SETUP WEBCAM SO THAT IT WORKS AND STUFF

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Robot {
+
+    //initialize all the parts of the robot here
+    public DcMotor RightFront = null;
+    public DcMotor RightBack = null;
+    public DcMotor LeftFront = null;
+    public DcMotor LeftBack = null;
+    public BNO055IMU imu;
+    public double speedConstant = 1;
+
+    HardwareMap hardwareMap = null;
+    public ElapsedTime runtime = new ElapsedTime();
+
+    public Robot(HardwareMap hwMap){
+        initialize(hwMap);
+    }
+
+    private void initialize(HardwareMap hwMap){
+        hardwareMap = hwMap;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        // Technically this is the default, however specifying it is clearer
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        // Without this, data retrieving from the IMU throws an exception
+        imu.initialize(parameters);
+
+        //Assign all the Parts of the Robot Here
+        RightFront = hardwareMap.dcMotor.get("RightFront"); // 0
+        RightBack = hardwareMap.dcMotor.get("RightBack"); // 1
+        LeftFront = hardwareMap.dcMotor.get("LeftFront"); // 2
+        LeftBack = hardwareMap.dcMotor.get("LeftBack"); // 3
+
+        //Set Motor Directions
+        RightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        RightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        LeftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        LeftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Set Motor Mode
+        RightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        RightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LeftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //Set Zero Power Behavior
+        RightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LeftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LeftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Set Motor Powers to 0
+        RightFront.setPower(0);
+        RightBack.setPower(0);
+        LeftFront.setPower(0);
+        LeftBack.setPower(0);
+    }
+}
